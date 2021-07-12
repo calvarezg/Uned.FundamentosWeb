1104994cf9172cfc4511e33df2d9f0daf8563ec3
Carlos <calvarezgcr@gmail.com>
2021-07-12 07:56:27 -0600

    Basic list of items (data pulled from static collection)

---
+++ MusicStore/MusicStore/Controllers/GuitarController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Controllers
+{
+    public class GuitarController : Controller
+    {
+        public ActionResult Index()
+        {            
+            return View(GuitarColletction);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        private static List<Guitar> guitars = null;
+        private static List<Guitar> GuitarColletction
+        {
+            get
+            {
+                if (guitars == null)
+                {
+                    guitars = new List<Guitar>();
+                    guitars.Add(new Guitar() { Name = "Standard Strat.", ImagePath = "img/img-1.jpg", Price = 1600 });
+                    guitars.Add(new Guitar() { Name = "Rosewood Std.", ImagePath = "img/img-2.jpg", Price = 1850 });
+                    guitars.Add(new Guitar() { Name = "Limited Edition", ImagePath = "img/img-3.jpg", Price = 2665 });
+                    guitars.Add(new Guitar() { Name = "Standard Mapple", ImagePath = "img/img-4.jpg", Price = 1500 });
+                }
+
+                return guitars;
+            }
+        }
+
+        private IEnumerable<Guitar> GetGuitarsByName(string filter)
+        {
+            var guitars = from guitar in GuitarColletction
+                          where guitar.Name.Contains(filter.Trim())
+                          select guitar;
+            return guitars.ToList();
+        }
+    }
+}
---
+++ MusicStore/MusicStore/Models/Guitar.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Models
+{
+    public class Guitar
+    {
+        private static int LastId = 0;
+
+        public int Id { get; private set; }
+
+        [DisplayName("Model")]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Image path:")]
+        [RegularExpression(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")]
+        public string ImagePath { get; set; }
+
+        [DisplayName("Item Price")]
+        public double Price { get; set; }
+
+        public Guitar()
+        {
+            Id = LastId++;
+        }
+    }
+}
